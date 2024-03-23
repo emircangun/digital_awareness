982f836f0145f0f274b5b263960736404997271a
emircangun <gundogdu.emircan@outlook.com>
2024-03-23 21:30:07 +0300

    Fix question class bug

--- app.py
+++ app.py
@@ -1,14 +1,18 @@
 from flask import Flask
 
+from model.questions_model import q, QuestionsClass
 from controller import main_controller
 import config
 
 
-def create_app():    
+def create_app():
     app = Flask(__name__)
     app.add_url_rule("/", view_func=main_controller.main_page, methods=["GET", "POST"])
     app.add_url_rule("/questions/<int:url_id>", view_func=main_controller.question_page, methods=["GET", "POST"])
     app.add_url_rule("/pairwise-comparison", view_func=main_controller.pairwise_comp_page, methods=["GET", "POST"])
+
+    q = QuestionsClass()
+
     return app
 
 

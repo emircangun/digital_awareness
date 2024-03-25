39b0179bab3f04d9d12ebb5b2cfd3ee3b0784406
emircangun <gundogdu.emircan@outlook.com>
2024-03-25 15:28:58 +0300

    Fix session bug

--- app.py
+++ app.py
@@ -9,6 +9,7 @@ import config
 def create_app():
     app = Flask(__name__)
 
+    app.secret_key = '1773'
     app.config['SESSION_TYPE'] = 'filesystem'
     app.config['PERMANENT_SESSION_LIFETIME'] = timedelta(hours=3)
     
@@ -16,10 +17,12 @@ def create_app():
     app.add_url_rule("/questions/<int:url_id>", view_func=main_controller.question_page, methods=["GET", "POST"])
     app.add_url_rule("/pairwise-comparison", view_func=main_controller.pairwise_comp_page, methods=["GET", "POST"])
     
+    Session(app)
+
     return app
 
 app = create_app()
-Session(app)
+# Session(app)
 
 if __name__ == "__main__":
     app.run(host=config.LOCALHOST_IP, port=config.PORT, debug=True)
